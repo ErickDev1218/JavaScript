6dfdcd40d408dcc0c1d086fa578eb8426234d890
Antonio Erick Freitas Ferreira <erickpdm879@gmail.com>
2022-02-02 18:15:25 -0300

    Estudos dia 02/02

---
+++ JavaScript/5. Funções/functions_1.js
@@ -0,0 +1,22 @@
+// Criar um aplicativo de frases motivacionais
+
+/*console.log('Estudar é muito bom')
+console.log('Paciência e persistência')
+console.log('Revisão é a mãe do aprendizado')*/
+
+// Declaração da função ou function statement
+function createPhrases() {
+    // Aqui é um novo escopo
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
+
+// Executar a função ou execute, run, call or invoke
+
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+
+console.log('Fim do programa')
---
+++ JavaScript/5. Funções/functions_4.js
@@ -0,0 +1,11 @@
+// Function hoisting
+
+sayMyName();
+
+
+function sayMyName() {
+    console.log('Erick')
+    
+}
+
+// Atenção pois nao funcionaria caso eu usasse como uma expressão utilizando de 'var' 'let' ou 'const'
